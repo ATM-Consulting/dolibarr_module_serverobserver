31df7e872817e9018c8a57d193c150d0c72ffe62
Maxime Kohlhaas <maxime@atm-consulting.fr>
2020-05-25 14:57:49 +0200

    NEW : add details about modules

--- _instance/check.php
+++ _instance/check.php
@@ -9,77 +9,148 @@
 	require $dir.'master.inc.php';
 
 	$result = new stdClass;
-	
+
+	$result->apiversion = '1.0';
+
 	$result->dolibarr = new stdClass;
 	$result->dolibarr->version = DOL_VERSION;
 	$result->dolibarr->version1 = $conf->global->MAIN_VERSION_LAST_INSTALL;
 	$result->dolibarr->theme = $conf->theme;
-	
+
 	$result->dolibarr->path=new stdClass;
-	$result->dolibarr->path->http = dol_buildpath('/',2); 
+	$result->dolibarr->path->http = dol_buildpath('/',2);
 	$result->dolibarr->path->relative = dol_buildpath('/var/www/client/',1);
 	$result->dolibarr->path->absolute = dol_buildpath('/var/www/client/',0);
-	
+
 	$result->dolibarr->data = new stdClass;
 	$result->dolibarr->data->path = DOL_DATA_ROOT;
 	$result->dolibarr->data->size = _dir_size(DOL_DATA_ROOT);
-	
+
 	$result->dolibarr->htdocs=new stdClass;
 	$result->dolibarr->htdocs->path = DOL_DOCUMENT_ROOT;
 	$result->dolibarr->htdocs->size = _dir_size(DOL_DOCUMENT_ROOT);
-	
+
 	$result->dolibarr->repertoire_client=new stdClass;
 	$result->dolibarr->repertoire_client->path = dirname(dirname(DOL_DOCUMENT_ROOT));
 	$result->dolibarr->repertoire_client->size = _dir_size($result->dolibarr->repertoire_client->path);
-	
+
 	$result->db=new stdClass;
 	$result->db->host = $dolibarr_main_db_host;
 	$result->db->name = $dolibarr_main_db_name;
 	$result->db->user = $dolibarr_main_db_user;
 	$result->db->type = $dolibarr_main_db_type;
-	
+
 	$result->user=new stdClass;
 	$result->user->all = _nb_user();
 	$result->user->active = _nb_user(true);
 	$result->user->date_last_login = _last_login() ;
-	
+
 	$result->module = new stdClass;
-	
+
 	$result->module = _module_active();
-	
+
 	$result->server_info = _getServerInfo();
 	//var_dump($result);
-	
+
 	echo json_encode($result);
-	
+
 function _module_active() {
-	
-	global $db;
-	
-	$sql="SELECT name FROM ".MAIN_DB_PREFIX."const WHERE name LIKE 'MAIN_MODULE_%' AND value=1 ORDER BY name";
-	
-	$res = $db->query($sql);
-	$Tab=array();
-	while($obj = $db->fetch_object($res)) {
-		if(preg_match('/^MAIN_MODULE_([0-9A-Z]+)$/i',$obj->name,$reg)) {
-			$name =ucfirst(strtolower($reg[1]));
-			
-			if(!in_array($name,$Tab)) $Tab[] = $name;
+	include_once DOL_DOCUMENT_ROOT . '/core/lib/functions2.lib.php';
+
+	global $db, $conf;
+	$modNameLoaded = array();
+	$modulesdir = dolGetModulesDirs();
+
+	foreach ($modulesdir as $dir)
+	{
+		$handle = @opendir($dir);
+		if (is_resource($handle))
+		{
+			while (($file = readdir($handle)) !== false)
+			{
+				if (is_readable($dir.$file) && substr($file, 0, 3) == 'mod' && substr($file, dol_strlen($file) - 10) == '.class.php')
+				{
+					$modName = substr($file, 0, dol_strlen($file) - 10);
+
+					if ($modName)
+					{
+						/*if (!empty($modNameLoaded[$modName]))   // In cache of already loaded modules ?
+						{
+							$mesg = "Error: Module ".$modName." was found twice: Into ".$modNameLoaded[$modName]." and ".$dir.". You probably have an old file on your disk.<br>";
+							setEventMessages($mesg, null, 'warnings');
+							dol_syslog($mesg, LOG_ERR);
+							continue;
+						}*/
+
+						try
+						{
+							$res = include_once $dir.$file; // A class already exists in a different file will send a non catchable fatal error.
+							if (class_exists($modName))
+							{
+								try {
+									$objMod = new $modName($db);
+									$modNameLoaded[$modName] = new stdClass();
+									$modNameLoaded[$modName]->dir = $dir;
+									$modNameLoaded[$modName]->numero = $objMod->numero;
+									$modNameLoaded[$modName]->version = $objMod->version;
+									$modNameLoaded[$modName]->source = $objMod->isCoreOrExternalModule();
+									$modNameLoaded[$modName]->gitinfos = _getModuleGitInfos($dir);
+									$modNameLoaded[$modName]->editor_name = dol_escape_htmltag($objMod->getPublisher());
+									$modNameLoaded[$modName]->editor_url = dol_escape_htmltag($objMod->getPublisherUrl());
+									$modNameLoaded[$modName]->active = !empty($conf->global->{$objMod->const_name});
+								}
+								catch (Exception $e)
+								{
+									dol_syslog("Failed to load ".$dir.$file." ".$e->getMessage(), LOG_ERR);
+								}
+							}
+							else
+							{
+								print "Warning bad descriptor file : ".$dir.$file." (Class ".$modName." not found into file)<br>";
+							}
+						}
+						catch (Exception $e)
+						{
+							dol_syslog("Failed to load ".$dir.$file." ".$e->getMessage(), LOG_ERR);
+						}
+					}
+				}
+			}
+			closedir($handle);
+		}
+		else
+		{
+			dol_syslog("htdocs/admin/modules.php: Failed to open directory ".$dir.". See permission and open_basedir option.", LOG_WARNING);
 		}
 	}
-	
-	return $Tab;
-}	
-	
+
+	return $modNameLoaded;
+}
+
+function _getModuleGitInfos($dir) {
+	global $donedir;
+	if(isset($donedir[$dir])) return $donedir[$dir];
+
+	$cmd = 'cd ' . $dir . ' && git status';
+	$res = shell_exec($cmd);
+
+	$branch = substr($res, strpos($res, 'On branch ')+10, strpos($res, "\n" )-10);
+	$donedir[$dir] = new stdClass();
+	$donedir[$dir]->branch = $branch;
+	$donedir[$dir]->status = $res;
+
+	return $donedir[$dir];
+}
+
 function _dir_size($dir) {
-	
+
 	// taile en Mo
-	
+
 	$io = popen ( 'du -sm ' . $dir, 'r' );
     $size = fgets ( $io, 4096);
     $size = substr ( $size, 0, strpos ( $size, "\t" ) );
     pclose ( $io );
-	
+
 	return (int)$size;
 }
 
@@ -96,24 +167,24 @@ function _last_login() {
         return $obj->datelastlogin;
 
 }
-	
+
 function _nb_user($just_actif = false) {
 	global $db;
-	
+
 	$sql = "SELECT count(*) as nb FROM ".MAIN_DB_PREFIX."user WHERE 1 ";
-	
+
 	if($just_actif) {
 		$sql.=" AND statut=1 ";
 	}
-	
+
 	$res = $db->query($sql);
-	
+
 	$obj = $db->fetch_object($res);
-	
+
 	return (int)$obj->nb;
-	
-	
-}	
+
+
+}
 
 
 function _getServerInfo()
